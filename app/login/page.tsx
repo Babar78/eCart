f79be8fa314bf15ff04576c90585eb3e2219afec
Babar78 <muhammadbabarashraf8@gmail.com>
2024-07-05 03:24:18 +0500

    Update background image URL in login and signup pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div className="lg:bg-white lg:grid lg:grid-cols-7 gap-20 h-screen w-screen overflow-hidden loginOuterDiv">
+    <div className="lg:bg-white lg:grid lg:grid-cols-7 gap-20 h-screen w-screen overflow-hidden loginOuterDiv sm:p-0 p-10">
       <section className="col-span-4 lg:flex hidden items-center">
         <Image
           src={"/assets/authPageImg.svg"}
@@ -17,9 +17,20 @@ const LoginPage = () => {
           height={780}
         />
       </section>
-      <section className="lg:col-span-3 lg:flex flex-col justify-center lg:items-start items-center lg:bg-transparent bg-white lg:rounded-none rounded-lg">
-        <form action="" className="w-[370px] mt-10 space-y-10 pr-[20px]">
-          <div>
+      <section className="lg:col-span-3 lg:flex flex-col justify-center lg:items-start items-center lg:bg-transparent bg-white lg:rounded-none rounded-lg lg:p-0 p-10 sm:w-fit w-full">
+        <div className="lg:block flex items-center justify-center">
+          <Image
+            src="/assets/logo/logo1.png"
+            alt="Logo"
+            width={150}
+            height={100}
+          />
+        </div>
+        <form
+          action=""
+          className="sm:w-[370px] w-full mt-5 space-y-10 lg:pr-[20px]"
+        >
+          <div className="lg:text-start text-center">
             <h5 className="font-semibold text-[36px]">Login to eCart</h5>
             <span>Enter Your Details Below</span>
           </div>
@@ -57,7 +68,7 @@ const LoginPage = () => {
             size="md"
           />
         </form>
-        <div className="max-w-[370px] mt-5">
+        <div className="lg:w-[370px] w-full mt-5">
           <p className="text-center">
             Don't have an account?{" "}
             <Link href="/signup" className="text-orange-400">
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const Signup = () => {
   return (
-    <div className="lg:bg-white lg:grid lg:grid-cols-7 gap-20 h-screen w-screen overflow-hidden loginOuterDiv">
+    <div className="lg:bg-white lg:grid lg:grid-cols-7 gap-20 h-screen w-screen overflow-hidden loginOuterDiv sm:p-0 p-10">
       <section className="col-span-4 lg:flex hidden items-center">
         <Image
           src={"/assets/authPageImg.svg"}
@@ -17,9 +17,17 @@ const Signup = () => {
           height={780}
         />
       </section>
-      <section className="lg:col-span-3 lg:flex flex-col justify-center lg:items-start items-center lg:bg-transparent bg-white lg:rounded-none rounded-lg">
-        <form action="" className="w-[370px] mt-10 space-y-10 pr-[20px]">
-          <div>
+      <section className="lg:col-span-3 lg:flex flex-col justify-center lg:items-start items-center lg:bg-transparent bg-white lg:rounded-none rounded-lg lg:p-0 p-10 sm:w-fit w-full">
+        <div className="lg:block flex items-center justify-center">
+          <Image
+            src="/assets/logo/logo1.png"
+            alt="Logo"
+            width={150}
+            height={100}
+          />
+        </div>
+        <form action="" className="sm:w-[370px] mt-5 space-y-10 lg:pr-[20px]">
+          <div className="lg:text-start text-center">
             <h5 className="font-semibold text-[36px]">Create an Account</h5>
             <span>Enter Your Details Below</span>
           </div>
@@ -80,7 +88,7 @@ const Signup = () => {
             size="md"
           />
         </form>
-        <div className="w-[370px] mt-5">
+        <div className="sm:w-[370px] w-full mt-5">
           <p className="text-center">
             Don't have an account?{" "}
             <Link href="/login" className="text-orange-400">
